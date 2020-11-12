62156df0b91806d924c7aa0fa0567400de3fdb47
LorellaRM <lorella.1892@gmail.com>
2020-11-12 14:21:51 +0000

    Random excuse

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["One horse", "My dog", "My brother", "My grandfather"];
+  let what = ["broke", "stole", "ran over", "hid"];
+  let objet = ["my laptop", "the keys", "the car"];
+  let when = [
+    "while I was getting ready",
+    "on the way to work",
+    "when I finished my lunch",
+    "in the middle of the night",
+    "in front of my eyes"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let objetIndex = Math.floor(Math.random() * objet.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let ExcSentence =
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    objet[objetIndex] +
+    " " +
+    when[whenIndex] +
+    ".";
+
+  document.querySelector("#excuses").innerHTML = ExcSentence;
 };
